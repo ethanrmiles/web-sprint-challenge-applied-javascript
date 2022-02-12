a9ce3fabdb30254a64644c914c9f7fd3588b235a
Ethan Miles <ethanmilescs@gmail.com>
2022-02-12 13:57:15 -0600

    completed tasks 4 and 5. FINALLY!!!

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,30 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   const tabsDiv = document.createElement('div');
-  const tabItems = document.createElement('div');
-  
-  tabItems.classList.add('tab');
   tabsDiv.classList.add('topics');
 
 topics.forEach(topic => {
  const content = topic
+ const tabItems = document.createElement('div');
+ tabItems.classList.add('tab');
+ tabsDiv.appendChild(tabItems)
  tabItems.textContent = content
-  console.log("content", tabItems);
 })
   return tabsDiv;
 }
-// Tabs(['a', 'b'])
-Tabs(['javascript', 'bootstrap', 'technology'])
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+axios.get('http://localhost:5000/api/topics')
+.then(resp => {
+  const injection = resp.data.topics
+ const entryPoint = document.querySelector(selector)
+  entryPoint.appendChild(Tabs(injection))
+})
+.catch(err => {
+  console.error(err)
+})
+.finally(() => console.log('done'))
 }
 
 export { Tabs, tabsAppender }
